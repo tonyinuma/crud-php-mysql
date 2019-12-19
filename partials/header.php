0ea1ee849695d9de459d8d4de71f50f844dbcd86
tonyinuma <tony.inuma@gmail.com>
2019-12-19 17:02:49 -0500

    Feat: Navbar app

---
+++ partials/header.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta http-equiv="Content-Type" content="text/html;charset=UTF-8">
+    <title>CRUD PHP</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+</head>
+<body>
+
+<nav class="navbar navbar-dark bg-dark">
+    <div class="container">
+        <a href="../index.php" class="navbar-brand">CRUD PHP - MYSQL</a>
+    </div>
+</nav>
+
+
