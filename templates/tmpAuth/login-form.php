729de9aaf154fcfb9b3a09f9b0b031adb88e942d
tonyinuma <tony.inuma@gmail.com>
2019-12-20 12:58:22 -0500

    Add: Login Form - I forgot that xd

---
+++ templates/tmpAuth/login-form.php
@@ -0,0 +1,18 @@
+<?php include("../../partials/header.php") ?>
+
+<div class="container mt-5 ml-5">
+    <div class="row">
+        <form action="../../stmt/Auth/LoginEmployee.php" method="post">
+            <div class="form-group">
+                <label for="exampleInputEmail1">Email address</label>
+                <input type="email" class="form-control" name="email" aria-describedby="emailHelp">
+                <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
+            </div>
+            <div class="form-group">
+                <label for="exampleInputPassword1">Password</label>
+                <input type="password" class="form-control" name="clave">
+            </div>
+            <button type="submit" class="btn btn-primary">Entrar</button>
+        </form>
+    </div>
+</div>
